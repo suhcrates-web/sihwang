54a54dbcd8dfa485e73f2cfdbe5e0e03498a11df
suhcrates <suhcrates@hanmail.net>
2021-04-02 16:56:11 +0900

    asdf

--- jonghap.py
+++ jonghap.py
@@ -14,14 +14,12 @@ def jonghap(magam=False):
         bg = background_magam()
     kospi_toojaja = kos_toojaja('kospi', bg['kospi_plma'])
     time.sleep(3)
-
     kosdaq_toojaja = kos_toojaja('kosdaq',bg['kosdaq_plma'])
-    time.sleep(3)
-    kosdaq_jongmok = kos_sentences('kosdaq', bg['kosdaq_plma'])
     time.sleep(3)
     kospi_upjong = upjong_maker('kospi',bg['kospi_plma'])
     kospi_jongmok = kos_sentences('kospi', bg['kospi_plma'])
     kosdaq_upjong = upjong_kosdaq(bg['kosdaq_plma'])
+    kosdaq_jongmok = kos_sentences('kosdaq', bg['kosdaq_plma'])
     ment = f"""<br><br>{bg['kospi_ment']}<br><br>{kospi_toojaja}<br><br>{kospi_jongmok}\
 <br><br>{kospi_upjong}<br><br>{bg['kosdaq_ment']}<br><br>{kosdaq_toojaja}<br><br>{kosdaq_jongmok}\
 <br><br>{kosdaq_upjong}<br><br>{bg['exch_ment']}<br><br>"""
