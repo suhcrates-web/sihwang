3a91a185e3ccfcdbdf43ddf1e8bb4f15b5deb5d5
suhcrates <suhcrates@hanmail.net>
2021-05-31 12:35:25 +0900

    asdf

--- jonghap.py
+++ jonghap.py
@@ -15,8 +15,8 @@ def jonghap(magam=False, version ='1'):
     with open(f'data/{jong_time}.csv', 'w') as f:
         f.writelines([now])
     if not magam:
-        # bg = background()
-        pass
+        bg = background()
+        # pass
     elif magam:
         bg = background_magam()
 
